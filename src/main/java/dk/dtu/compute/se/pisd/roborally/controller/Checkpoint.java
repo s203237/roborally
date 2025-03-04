6903a7285bb7277f019a9898e01044867648906d
Camellia <91070768+s203237@users.noreply.github.com>
2025-03-04 11:17:44 +0100

    moved Checkpoint.java to controller

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
@@ -1,7 +1,9 @@
-package dk.dtu.compute.se.pisd.roborally.model;
+package dk.dtu.compute.se.pisd.roborally.controller;
 
 import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
 import org.jetbrains.annotations.NotNull;
 
 public class Checkpoint extends FieldAction {
