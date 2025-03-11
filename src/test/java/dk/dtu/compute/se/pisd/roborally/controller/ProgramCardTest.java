a3d4118f424ebd0a185ea883f9f8bc45669c1352
Camellia <91070768+s203237@users.noreply.github.com>
2025-03-11 08:53:12 +0100

    done with test

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/ProgramCardTest.java
@@ -0,0 +1,35 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ProgramCardTest {
+    private final int TEST_WIDTH = 8;
+    private final int TEST_HEIGHT = 8;
+
+    private GameController gameController;
+
+    @BeforeEach
+    void setUp() {
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        gameController = new GameController(board);
+        for (int i = 0; i < 6; i++) {
+            Player player = new Player(board, null,"Player " + i);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, i));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+        board.setCurrentPlayer(board.getPlayer(0));
+    }
+
+    @AfterEach
+    void tearDown() {
+        gameController = null;
+    }
+
+
+
+}
